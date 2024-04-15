5ea43b87845dd45f4cfe812be127e4c82dfe9dee
LucasSiqueira06 <lucassiqueira1106@gmail.com>
2024-04-15 20:59:23 -0300

    feat: verification of player jump

--- Player.gd
+++ Player.gd
@@ -8,21 +8,31 @@ var facingLeft : bool = false
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += currentGravity * delta
+	else:
+		velocity.y = 0
 	
 	velocity.x = 0
+	
 	if Input.is_key_pressed(KEY_LEFT):
-		velocity.x -= moveSpeed
 		if not facingLeft:
 			$AnimatedSprite2D.scale.x *= -1
 			facingLeft = true
+		velocity.x -= moveSpeed
 		$AnimatedSprite2D.play("Run")
 	elif Input.is_key_pressed(KEY_RIGHT):
-		velocity.x += moveSpeed
 		if facingLeft:
 			$AnimatedSprite2D.scale.x *= -1
 			facingLeft = false
+		velocity.x += moveSpeed
 		$AnimatedSprite2D.play("Run")
 	else:
-		$AnimatedSprite2D.play("Idle")
+		if is_on_floor():
+			$AnimatedSprite2D.play("Idle")
+	
+	if is_on_floor() and Input.is_key_pressed(KEY_SPACE):
+		velocity.y = -jumpForce
+		$AnimatedSprite2D.play("Jump")
+	elif velocity.y < 0 and $AnimatedSprite2D.animation != "Jump":
+		$AnimatedSprite2D.play("Jump")
+	
 	move_and_slide()
-
