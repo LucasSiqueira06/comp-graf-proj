ade2afe49cfcfa235df34b784f3489acfdb11524
LucasSiqueira06 <lucassiqueira1106@gmail.com>
2024-04-15 19:39:42 -0300

    work in progress: player movement

--- Player.gd
+++ Player.gd
@@ -6,4 +6,11 @@ var currentGravity : float = 500.0
 
 func _physics_process(delta):
 	if not is_on_floor():
-		pass
+		velocity.y += currentGravity * delta
+	
+	velocity.x = 0
+	
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= moveSpeed
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += moveSpeed
